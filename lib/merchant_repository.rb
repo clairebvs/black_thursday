432b33e27fd6cf9a9ce07b07abc00771a4331cce
ClaireBvs <clairembeauvais@gmail.com>
2018-05-26 16:02:03 -0600

    refactor hash table for merchant repository class

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -7,9 +7,16 @@ class MerchantRepository
   end
 
   def build_hash_table
-    @id = @repository.group_by { |item| item.id }
-    @name = @repository.group_by { |item| item.name }
-    @created_at = @repository.group_by { |item| item.created_at }
-    @updated_at = @repository.group_by { |item| item.updated_at }
+    @id = @repository.group_by { |merchant| merchant.id }
+    @name = @repository.group_by { |merchant| merchant.name }
+    @created_at = @repository.group_by { |merchant| merchant.created_at }
+    @updated_at = @repository.group_by { |merchant| merchant.updated_at }
   end
+
+  def find_by_id(id)
+    @repository.find_all do |merchant|
+      merchant.id
+    end
+  end
+
 end
