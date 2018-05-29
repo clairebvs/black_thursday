4bb7d26ab2884ed516f9d0fed1c47fd35b64f1af
JP Lynch <JPLynch35@gmail.com>
2018-05-29 17:19:54 -0600

    adds customer_respository and customer Classes

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,6 +2,8 @@ require_relative 'file_loader'
 require_relative 'item_repository'
 require_relative 'merchant_repository'
 require_relative 'invoice_repository'
+require_relative 'transaction_repository'
+require_relative 'customer_repository'
 require_relative 'sales_analyst'
 
 class SalesEngine
@@ -25,6 +27,14 @@ class SalesEngine
 
   def invoices
     @invoices ||= InvoiceRepository.new(open_items_csv(@file_paths[:invoices]), self)
+  end
+
+  def transactions
+    @transactions ||= TransactionRepository.new(open_items_csv(@file_paths[:transactions]), self)
+  end
+
+  def customers
+    @customers ||= CustomerRepository.new(open_items_csv(@file_paths[:customers]), self)
   end
 
   def analyst
