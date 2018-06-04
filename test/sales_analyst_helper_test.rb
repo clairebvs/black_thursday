2db473e3372d03c61c999fe5892b455548f9d6ed
JP Lynch <JPLynch35@gmail.com>
2018-06-04 14:00:12 -0600

    adds the remainder of tests for sales analyst helper

--- test/sales_analyst_helper_test.rb
+++ test/sales_analyst_helper_test.rb
@@ -101,26 +101,36 @@ class SalesAnalystHelperTest < Minitest::Test
   end
 
   def test_can_calculate_days_of_week_per_invoice
+    all_invoices = @engine.invoices.all
     actual = @sales_analyst.days_of_week_per_invoice(all_invoices)
-    assert_equal 52, actual.length
-    assert_instance_of Merchant, actual.first
+    assert_equal 7, actual.length
+    assert_equal 708, actual[0].length
   end
 
-  # def test_can_calculate_merchants_with_high_item_count
-  #   actual = @sales_analyst.merchants_with_high_item_count
-  #   assert_equal 52, actual.length
-  #   assert_instance_of Merchant, actual.first
-  # end
+  def test_can_calculate_invoices_per_day_of_week
+    all_invoices = @engine.invoices.all
+    days_of_week = @sales_analyst.days_of_week_per_invoice(all_invoices)
+    group_days_of_week = days_of_week.values
+    actual = @sales_analyst.invoices_per_day_of_week(group_days_of_week)
+    assert_equal 7, actual.length
+    assert_equal 729, actual[0]
+  end
 
-  # def test_can_calculate_merchants_with_high_item_count
-  #   actual = @sales_analyst.merchants_with_high_item_count
-  #   assert_equal 52, actual.length
-  #   assert_instance_of Merchant, actual.first
-  # end
+  def test_can_calculate_days_with_high_invoice_count
+    all_invoices = @engine.invoices.all
+    days_of_week = @sales_analyst.days_of_week_per_invoice(all_invoices)
+    group_days_of_week = days_of_week.values
+    invoices_per_day = @sales_analyst.invoices_per_day_of_week(group_days_of_week)
+    average_invoices = @sales_analyst.calculate_mean(invoices_per_day).round(2)
+    standard_deviation = @sales_analyst.standard_deviation(invoices_per_day)
+    actual = @sales_analyst.days_with_high_invoice_count(days_of_week, invoices_per_day, standard_deviation, average_invoices)
+    assert_equal 'Wednesday', actual.first
+  end
 
-  # def test_can_calculate_merchants_with_high_item_count
-  #   actual = @sales_analyst.merchants_with_high_item_count
-  #   assert_equal 52, actual.length
-  #   assert_instance_of Merchant, actual.first
-  # end
+  def test_can_calculate_invoice_item_totals
+    invoice_id = 77
+    invoice_items_with_invoice_id = @engine.invoice_items.find_all_by_invoice_id(invoice_id)
+    actual = @sales_analyst.invoice_item_totals(invoice_items_with_invoice_id)
+    assert_equal 2353.56, actual.to_f
+  end
 end
